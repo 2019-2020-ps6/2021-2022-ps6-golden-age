c88a3e6f31ab76d344b2a1602e974638d9942e59
ClervieCauser <clervie.causer@etu.unice.fr>
2022-03-22 08:21:20 +0100

    premier pas

---
+++ projet/front-end/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
+import {HeaderUserComponent} from './header/headerUser/headerUser.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderUserComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
