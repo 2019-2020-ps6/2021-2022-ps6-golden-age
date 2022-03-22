455847a869a59ef3b717271971f4d21206c0d711
bastien <bastien.bezes@etu.unice.fr>
2022-03-22 11:42:31 +0100

    modification user
    #26

--- projet/front-end/src/app/app.routing.module.ts
+++ projet/front-end/src/app/app.routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { helpPageProComponent } from './help/pro/helpPagePro.component';
+import { helpPageUserComponent } from './help/user/helpPageUser.component';
 
 const routes: Routes = [
-    /*{path: 'user-list', component: UserListComponent},
+    {path: 'helpPro', component: helpPageProComponent},
+    {path: 'helpUser', component: helpPageUserComponent},
+    /*
     {path: 'quiz-list', component: QuizListComponent},
     {path: 'edit-quiz/:id', component: EditQuizComponent},
     { path: '', redirectTo: '/quiz-list', pathMatch: 'full' },*/
