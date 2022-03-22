f02e349b0abf2aa1ceaa94ff653abc2306811d5f
bastien <bastien.bezes@etu.unice.fr>
2022-03-22 11:30:06 +0100

    component de la page d'aide
    #26

---
+++ projet/front-end/src/app/help/user/helpPageUser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './helpPageUser.component.html',
+  styleUrls: ['./helpPageUser.component.scss']
+})
+export class helpPageUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
