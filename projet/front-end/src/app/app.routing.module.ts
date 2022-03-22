f3677dcb11d8599f1a59833922dfaf4f736beb72
bastien <bastien.bezes@etu.unice.fr>
2022-03-22 11:57:33 +0100

    page d'accueil user
    #12

--- projet/front-end/src/app/app.routing.module.ts
+++ projet/front-end/src/app/app.routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { helpPageProComponent } from './help/pro/helpPagePro.component';
 import { helpPageUserComponent } from './help/user/helpPageUser.component';
+import { AccueilUserComponent } from './accueil/user/accueilUser.component';
 
 const routes: Routes = [
     {path: 'helpPro', component: helpPageProComponent},
     {path: 'helpUser', component: helpPageUserComponent},
+    {path: 'accueilUser', component: AccueilUserComponent},
+
     /*
     {path: 'quiz-list', component: QuizListComponent},
     {path: 'edit-quiz/:id', component: EditQuizComponent},
