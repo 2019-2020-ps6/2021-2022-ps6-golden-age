ea766d27587d9977826ab6c79b6829ffa087ca1a
Ludovic <lulubcancer@hotmail.fr>
2022-03-28 17:38:03 +0200

    Modification de app.modules
    #2

--- projet/front-end/src/app/app.module.ts
+++ projet/front-end/src/app/app.module.ts
@@ -6,34 +6,34 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 import {HeaderUserComponent} from './header/headerUser/headerUser.component';
-import {QuizComponent} from './quizzes/quiz/quiz.component';
-import {QuestionComponent} from './questions/question/question.component';
-import {QuestionListComponent} from './questions/question-list/question-list.component';
-import {QuestionFormComponent} from './questions/question-form/question-form.component';
-import {QuizFormComponent} from './quizzes/quiz-form/quiz-form.component';
-import {QuizListComponent} from './quizzes/quiz-list/quiz-list.component';
-import {HelpPageUserComponent} from './help/user/help-page-user.component';
-import {HelpPageProComponent} from './help/pro/help-page-pro.component';
-import {ShowQuizComponent} from './quizzes/show-quiz/show-quiz.component';
-import {ThemeListComponent} from './themes/theme-list/theme-list.component';
-import {ThemeFormComponent} from './themes/theme-form/theme-form.component';
 
+import { ThemeListComponent } from './themes/theme-list/theme-list.component';
+import { ThemeFormComponent } from './themes/theme-form/theme-form.component';
+import { ThemeComponent } from './themes/theme/theme.component';
+
+import { QuestionListComponent } from './questions/question-list/question-list.component';
+import { QuestionFormComponent } from './questions/question-form/question-form.component';
+import { QuestionComponent } from './questions/question/question.component';
+
+import { QuizListComponent } from './quizzes/quiz-list/quiz-list.component';
+import { QuizFormComponent } from './quizzes/quiz-form/quiz-form.component';
+import { QuizComponent } from './quizzes/quiz/quiz.component';
+import { EditQuizComponent } from './quizzes/edit-quiz/edit-quiz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderUserComponent,
-    QuizComponent,
-    QuizFormComponent,
-    QuizListComponent,
-    QuestionComponent,
-    QuestionListComponent,
-    QuestionFormComponent,
-    HelpPageUserComponent,
-    HelpPageProComponent,
-    ShowQuizComponent,
     ThemeListComponent,
     ThemeFormComponent,
+    ThemeComponent,
+    QuestionListComponent,
+    QuestionFormComponent,
+    QuestionComponent,
+    QuizListComponent,
+    QuizFormComponent,
+    QuizComponent,
+    EditQuizComponent
   ],
   imports: [
     BrowserModule,
