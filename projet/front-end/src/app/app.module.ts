b666442d7a088878e4af416375e17ec2c7f613f9
Ludovic <lulubcancer@hotmail.fr>
2022-03-28 22:00:49 +0200

    cadeau Aurélia
    #1

--- projet/front-end/src/app/app.module.ts
+++ projet/front-end/src/app/app.module.ts
@@ -6,18 +6,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 import {HeaderUserComponent} from './header/headerUser/headerUser.component';
-import {QuizComponent} from './quizzes/quiz/quiz.component';
-import {QuestionComponent} from './questions/question/question.component';
-import {QuestionListComponent} from './questions/question-list/question-list.component';
-import {QuestionFormComponent} from './questions/question-form/question-form.component';
-import {QuizFormComponent} from './quizzes/quiz-form/quiz-form.component';
-import {QuizListComponent} from './quizzes/quiz-list/quiz-list.component';
-import {HelpPageUserComponent} from './help/user/help-page-user.component';
-import {HelpPageProComponent} from './help/pro/help-page-pro.component';
-import {ShowQuizComponent} from './quizzes/show-quiz/show-quiz.component';
-import {ThemeListComponent} from './themes/theme-list/theme-list.component';
-import {ThemeFormComponent} from './themes/theme-form/theme-form.component';
-import {ThemeComponent} from './themes/theme/theme.component';
 
 import { ThemeListComponent } from './themes/theme-list/theme-list.component';
 import { ThemeFormComponent } from './themes/theme-form/theme-form.component';
